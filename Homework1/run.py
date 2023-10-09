1d86a4383c713725f76474b39c33309edbf49e27
ovankerm <otis.vankerm@student.uclouvain.be>
2023-10-09 23:10:55 +0200

    final homework 2

--- Homework1/run.py
+++ Homework1/run.py
@@ -1,10 +1,11 @@
 from matplotlib import pyplot as plt
 from math import cos
 
-file_suffix = "_cos.eps"
-SAVE_FIG = False
-PLOT_FIG = False
-PRINT_ERRORS = False
+file_suffix = "_runge_small_interval.eps"
+SAVE_FLOATER = True
+SAVE_LAGRANGE = True
+PLOT_FIG = True
+PRINT_ERRORS = True
 
 # ------ FUNCTIONS AND CLASSES ------
 def linspace(start, end, num):
@@ -35,7 +36,7 @@ def error(y_approx, y_exact):
         e_inf_norm = max(e_inf_norm, y_exact[i])
         e_2 += error_sq[i]
         e_2_norm += y_exact[i]**2
-    return error_sq, error_abs, e_2/e_2_norm, e_inf/e_inf_norm
+    return error_sq, error_abs, (e_2/e_2_norm)**(0.5), e_inf/e_inf_norm
 
 
 class Lagrange:
@@ -97,10 +98,11 @@ f1 = lambda x : cos(x)
 f2 = lambda x : 1/(1 + 25 * x*x)
 
 # ------ APPROX PARAM ------
-N = [5, 10, 15]
+N = [10, 30, 50]
 d = [0, 3, 5, 8]
 f = f2
-x = [linspace(-1, 1, N[0]), linspace(-1, 1, N[1]), linspace(-1, 1, N[2])]
+interval = 1
+x = [linspace(-interval, interval, N[0]), linspace(-interval, interval, N[1]), linspace(-interval, interval, N[2])]
 y = [[0 for i in range(N[0])], [0 for i in range(N[1])], [0 for i in range(N[2])]]
 for k in range(3):
     for i in range(N[k]):
@@ -108,7 +110,7 @@ for k in range(3):
 
 # ------ PLOTS PARAMS ------
 N_plot = 1000
-x_plot = linspace(-1.0000001, 1.0000001, N_plot)
+x_plot = linspace(-interval - 0.000001, interval + 0.000001, N_plot)
 y_plot_lagrange = [[0 for i in range(len(x_plot))] for j in range(3)]
 y_plot_fh = [[[0 for i in range(len(x_plot))] for j in range(3)] for k in range(4)]
 y_exact = [f(x_plot[i]) for i in range(N_plot)]
@@ -142,7 +144,7 @@ ax.set_xlabel('x')
 ax.set_ylabel('y')
 
 ax.set_title('Lagrange interpolation')
-if SAVE_FIG:
+if SAVE_LAGRANGE:
     fig.savefig('Homework1/images/Lagrange'+file_suffix, format='eps')
 
 # ------ PLOT FLOATER-HORMANN ------
@@ -158,7 +160,7 @@ for j in range(4):
     ax[j//2][j%2].set_ylabel('y')
 
 fig.suptitle('Floater-Hormann interpolation', y=0.93)
-if SAVE_FIG:
+if SAVE_FLOATER:
     fig.savefig('Homework1/images/Floater_Hormann'+file_suffix, format='eps')
 
 # ------ PLOT ERRORS ------
@@ -170,7 +172,7 @@ ax.set_xlabel('x')
 ax.set_ylabel('error')
 ax.legend()
 ax.set_title('Lagrange interpolation error')
-if SAVE_FIG:
+if SAVE_LAGRANGE:
     fig.savefig('Homework1/images/Lagrange_error'+file_suffix, format='eps')
 
 fig, ax = plt.subplots(2, 2, figsize=(14, 9.5))
@@ -185,7 +187,7 @@ for j in range(4):
     ax[j//2][j%2].set_ylabel('error')
 
 fig.suptitle('Floater-Hormann interpolation error', y=0.93)
-if SAVE_FIG:
+if SAVE_FLOATER:
     fig.savefig('Homework1/images/Floater_Hormann_error'+file_suffix, format='eps')
 
 if PLOT_FIG:
@@ -201,7 +203,7 @@ if PRINT_ERRORS:
 
     print("")
 
-    print("------ Floater-Hermann Interpolation Errors ------")
+    print("------ Floater-Hormann Interpolation Errors ------")
     for j in range(4):
         for i in range(3):
             if errors_fh[j][i] != None:
@@ -209,40 +211,46 @@ if PRINT_ERRORS:
 
 # f(x) = cos(x)
 # ------ Lagrange Interpolation Errors ------
-# N = 5  ---> e_2 error : 5.0293139021815035e-09, e_inf error : 0.0001270582001834927
-# N = 10  ---> e_2 error : 1.5823387619380798e-18, e_inf error : 3.395717409880349e-09
-# N = 15  ---> e_2 error : 2.5101484133139096e-29, e_inf error : 3.674840052609868e-14
+# N = 5  ---> e_2 error : 7.091764303273428e-05, e_inf error : 0.00012705820244046979
+# N = 10  ---> e_2 error : 1.2579102983679509e-09, e_inf error : 3.3957167437492626e-09
+# N = 15  ---> e_2 error : 5.215117652858044e-15, e_inf error : 4.1855428998041385e-14
 
-# ------ Floater-Hermann Interpolation Errors ------
-# (N, d) = (5, 0)  ---> e_2 error : 0.000853025117968096, e_inf error : 0.046063787008178605
-# (N, d) = (10, 0)  ---> e_2 error : 0.0008074530190083818, e_inf error : 0.03784570000213539
-# (N, d) = (15, 0)  ---> e_2 error : 8.401428856993519e-05, e_inf error : 0.01642730581980803
-# (N, d) = (5, 3)  ---> e_2 error : 3.3004189242267272e-06, e_inf error : 0.003047324184469707
-# (N, d) = (10, 3)  ---> e_2 error : 1.8686620981159516e-05, e_inf error : 0.008017343128908118
-# (N, d) = (15, 3)  ---> e_2 error : 3.887837969840777e-08, e_inf error : 0.0005198333202118483
-# (N, d) = (5, 5)  ---> e_2 error : 5.0293139021815035e-09, e_inf error : 0.0001270582001834927
-# (N, d) = (10, 5)  ---> e_2 error : 4.633357168351652e-05, e_inf error : 0.012954563669858645
-# (N, d) = (15, 5)  ---> e_2 error : 6.122741905468128e-08, e_inf error : 0.0006596923502273893
-# (N, d) = (10, 8)  ---> e_2 error : 6.482753063139127e-06, e_inf error : 0.003472334918531723
-# (N, d) = (15, 8)  ---> e_2 error : 2.2002947418139367e-05, e_inf error : 0.012642986971682979
+# ------ Floater-Hormann Interpolation Errors ------
+# (N, d) = (5, 0)  ---> e_2 error : 0.0292065884816612, e_inf error : 0.046063786763806534
+# (N, d) = (10, 0)  ---> e_2 error : 0.02841571269982499, e_inf error : 0.037845701507180175
+# (N, d) = (15, 0)  ---> e_2 error : 0.009165929218602807, e_inf error : 0.01642730959529956
+# (N, d) = (5, 3)  ---> e_2 error : 0.0018167051869256378, e_inf error : 0.0030473240870038175
+# (N, d) = (10, 3)  ---> e_2 error : 0.004322801669308307, e_inf error : 0.008017342282462793
+# (N, d) = (15, 3)  ---> e_2 error : 0.00019717597718646457, e_inf error : 0.0005198334421466187
+# (N, d) = (5, 5)  ---> e_2 error : 7.091764303273428e-05, e_inf error : 0.00012705820244046979
+# (N, d) = (10, 5)  ---> e_2 error : 0.006806875571818632, e_inf error : 0.012954562071191115
+# (N, d) = (15, 5)  ---> e_2 error : 0.0002474417051622173, e_inf error : 0.0006596924868016939
+# (N, d) = (10, 8)  ---> e_2 error : 0.0025461246497668357, e_inf error : 0.003472335036426721
+# (N, d) = (15, 8)  ---> e_2 error : 0.0046907291166925795, e_inf error : 0.012642987812234108
 
 
 
 # f(x) = 1/(25 * x*x)
 # ------ Lagrange Interpolation Errors ------
-# N = 5  ---> e_2 error : 0.49782471929229927, e_inf error : 0.4383607770453408
-# N = 10  ---> e_2 error : 0.07671685870683417, e_inf error : 0.3002920615239576
-# N = 15  ---> e_2 error : 20.044247658645283, e_inf error : 7.192503932487124
+# N = 5  ---> e_2 error : 0.7055669459089078, e_inf error : 0.43836078802986
+# N = 10  ---> e_2 error : 0.27697808295736354, e_inf error : 0.30029202419926715
+# N = 15  ---> e_2 error : 4.477080333444169, e_inf error : 7.192499218378267
 
-# ------ Floater-Hermann Interpolation Errors ------
-# (N, d) = (5, 0)  ---> e_2 error : 0.1787622012949484, e_inf error : 0.320367260147341
-# (N, d) = (10, 0)  ---> e_2 error : 0.015876383399105888, e_inf error : 0.16843787499409374
-# (N, d) = (15, 0)  ---> e_2 error : 0.00010936387920716343, e_inf error : 0.008573561688382622
-# (N, d) = (5, 3)  ---> e_2 error : 0.1768336870552963, e_inf error : 0.29325432035482735
-# (N, d) = (10, 3)  ---> e_2 error : 0.006039357238251586, e_inf error : 0.10768712757754066
-# (N, d) = (15, 3)  ---> e_2 error : 0.0003853339239288092, e_inf error : 0.014106937610770033
-# (N, d) = (5, 5)  ---> e_2 error : 0.49782471929229927, e_inf error : 0.4383607770453408
-# (N, d) = (10, 5)  ---> e_2 error : 0.011969426978701654, e_inf error : 0.1390015992377249
-# (N, d) = (15, 5)  ---> e_2 error : 0.0007921252366813573, e_inf error : 0.0235566336285566
-# (N, d) = (10, 8)  ---> e_2 error : 0.0063528468644932995, e_inf error : 0.11190892527529404
-# (N, d) = (15, 8)  ---> e_2 error : 0.00026553453567370873, e_inf error : 0.014214105780099665
+# ------ Floater-Hormann Interpolation Errors ------
+# (N, d) = (5, 0)  ---> e_2 error : 0.42280279065010107, e_inf error : 0.32036726128789667
+# (N, d) = (10, 0)  ---> e_2 error : 0.1260015208818021, e_inf error : 0.16843787496552026
+# (N, d) = (15, 0)  ---> e_2 error : 0.010457718599189782, e_inf error : 0.008573562129495034
+# (N, d) = (5, 3)  ---> e_2 error : 0.4205159753971341, e_inf error : 0.29325432184793354
+# (N, d) = (10, 3)  ---> e_2 error : 0.07771330122474897, e_inf error : 0.10768712755784186
+# (N, d) = (15, 3)  ---> e_2 error : 0.019629924132430667, e_inf error : 0.014106939223517065
+# (N, d) = (5, 5)  ---> e_2 error : 0.7055669459089078, e_inf error : 0.43836078802986
+# (N, d) = (10, 5)  ---> e_2 error : 0.1094048759325194, e_inf error : 0.1390015992135487
+# (N, d) = (15, 5)  ---> e_2 error : 0.028144719435802372, e_inf error : 0.02355663590370557
+# (N, d) = (10, 8)  ---> e_2 error : 0.07970474772988058, e_inf error : 0.11190892525497112
+# (N, d) = (15, 8)  ---> e_2 error : 0.016295230411251218, e_inf error : 0.01421410712077076
+
+# On the interval [-0.15, 0.15]
+# ------ Lagrange Interpolation Errors ------
+# N = 10  ---> e_2 error : 6.711810284068877e-05, e_inf error : 0.00018102507342724846
+# N = 30  ---> e_2 error : 7.999095601601501e-11, e_inf error : 8.498047059665013e-10
+# N = 50  ---> e_2 error : 1.76915685538325e-05, e_inf error : 0.00023658454310055872
